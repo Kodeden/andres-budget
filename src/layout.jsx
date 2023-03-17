79cdc5fd4dfd6cca2e058ca2e2308cb815c9f705
Manav Misra <manavm@visionwebsoft.com>
2023-03-17 18:18:41 -0500

    Add styling for h1

--- src/layout.jsx
+++ src/layout.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function Layout({ children }) {
   return (
     <>
-      <h1>The Budget App 💸</h1>
+      <h1 className="my-4 text-center text-2xl">The Budget App 💸</h1>
       {children}
     </>
   );
